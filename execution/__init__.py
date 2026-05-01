ba02e7503b1eb5212761125070c95bc656dbd258
Nims-S <nimsts@gmail.com>
2026-05-01 22:15:06 +0530

    Expose new execution components

--- execution/__init__.py
+++ execution/__init__.py
@@ -2,6 +2,7 @@ from .router import route_strategies, select_active_strategy
 from .allocator import allocate_capital
 from .drift_monitor import compare_performance
 from .live_bot import run_live_cycle, run_loop
+from .executor import TradeExecutor
 
 __all__ = [
     "route_strategies",
@@ -10,4 +11,5 @@ __all__ = [
     "compare_performance",
     "run_live_cycle",
     "run_loop",
+    "TradeExecutor",
 ]
