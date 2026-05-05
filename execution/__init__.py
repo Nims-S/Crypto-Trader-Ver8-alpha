31a67b2325008ced48985d311a7ddc8f7a06876c
Nims-S <nimsts@gmail.com>
2026-05-05 12:40:52 +0530

    Make execution package import-safe by removing eager live_bot import

--- execution/__init__.py
+++ execution/__init__.py
@@ -1,7 +1,6 @@
 from .router import route_strategies, select_active_strategy
 from .allocator import allocate_capital
 from .drift_monitor import compare_performance
-from .live_bot import run_live_cycle, run_loop
 from .executor import TradeExecutor
 
 __all__ = [
@@ -9,7 +8,5 @@ __all__ = [
     "select_active_strategy",
     "allocate_capital",
     "compare_performance",
-    "run_live_cycle",
-    "run_loop",
     "TradeExecutor",
 ]
